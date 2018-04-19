62db403cb692ef072d5834fda0b70a37daae939c
rzhang96 <rzhang1996@gmail.com>
2018-04-19 13:42:31 -0700

    peter please rebase before editing your stuff

--- backend/Drop.sql
+++ backend/Drop.sql
@@ -4,34 +4,42 @@ CREATE DATABASE DRAW;
 
 USE DRAW;
 
-CREATE TABLE User{
-	student_id INT UNSIGNED NOT NULL,
+CREATE TABLE User(
+	student_id INT UNSIGNED NOT NULL PRIMARY KEY,
+	name VARCHAR(256) NOT NULL,
 	room_preference VARCHAR(256) NOT NULL,
 	-- Need to assign timeslot as the user submits their form
 	timeslot INT UNSIGNED NOT NULL,
 	class VARCHAR(256) NOT NULL
-}
+);
 
-CREATE TABLE Reservation{
-	room_id INT UNSIGNED NOT NULL,
-	-- Make the student_id's
-	roomates
-	occupants VARCHAR (256) NOT NULL,
-	building_id INT UNSIGNED NOT NULL,
-	time_stamp INT UNSIGNED NOT NULL,
-}
-
-CREATE TABLE Room{
-	room_id INT UNSIGNED NOT NULL PRIMARY KEY,
-	building_id INT UNSIGNED NOT NULL,
-	capacity INT UNSINGED NOT NULL,
-	FOREIGN KEY (building_id) REFERENCES Building(building_id)
-}
 
 CREATE TABLE Building{
 	building_id INT UNSIGNED NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
-}
+);
+
+
+CREATE TABLE Room(
+	room_id INT UNSIGNED NOT NULL PRIMARY KEY,
+	building_id INT UNSIGNED NOT NULL,
+	capacity INT UNSIGNED NOT NULL,
+	FOREIGN KEY (building_id) REFERENCES Building(building_id)
+);
+
+CREATE TABLE Reservation(
+	res_id INT UNSIGNED NOT NULL AUTO INCREMENT PRIMARY KEY,
+	room_id INT UNSIGNED NOT NULL,
+	-- Make the student_id's
+	-- roomates 
+	-- Need to update this file and queries.php
+	owner INT UNSIGNED NOT NULL,
+	building_id INT UNSIGNED NOT NULL,
+	time_stamp INT UNSIGNED NOT NULL,
+	FOREIGN KEY (owner) REFERENCES User (student_id),
+	FOREIGN KEY(room_id) REFERENCES Room(room_id),
+	FOREIGN KEY (building_id) REFERENCES Building(building_id)
+);
 
 INSERT INTO Building (01, Wohlford)
 INSERT INTO Building (02, Boswell)
